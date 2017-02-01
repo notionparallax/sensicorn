149e445781668f0eb66cdb204e8525aec149c2f5
Ben Doherty <ben@notionparallax.co.uk>
2017-01-31 17:44:21 -0800

    some tidying I think

--- initialDataAnalysis/helpers.py
+++ initialDataAnalysis/helpers.py
@@ -1,44 +1,44 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from scipy.misc import imread
 from StringIO import StringIO
 import datetime
 import dateutil.parser
 import json
-import math
-import matplotlib
+# import math
+# import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
 import os
 import os.path
 import pandas as pd
-import pymongo
+# import pymongo
 import pytz
 import requests
 import signal
 import sys
-import time
-
+# import time
 
 
 SHOW_TESTS = False
 
-def getDFfromCSVURL(url,columnNames=False):
+
+def getDFfromCSVURL(url, columnNames=False):
     r = requests.get(url)
     data = r.content
     if columnNames:
-        return pd.read_csv(StringIO(data),header=0,names=columnNames)
+        return pd.read_csv(StringIO(data), header=0, names=columnNames)
     else:
         return pd.read_csv(StringIO(data))
+
 
 if SHOW_TESTS:
     print getDFfromCSVURL("https://docs.google.com/spreadsheets/d/1sijQZR2iFLo2FS_3r5gbsuAkaglRz557LWjdLxnPkpE/pub?gid=0&single=true&output=csv").head(3)
     print getDFfromCSVURL("https://docs.google.com/spreadsheets/d/1uE_tUItRANypaWxCmZeXWsgFZq_JDG626v4Gg2UQfKg/pub?gid=952022876&single=true&output=csv",
-                             columnNames=["a","b","c","d","e"]).head(3)
-
+                          columnNames=["a", "b", "c", "d", "e"]).head(3)
 
 
 def timeStats(df, showprint=True, actuallyReturn=True):
-    #This gives us an idea of the time domain that the data we have is interested in.
+    # This gives us an idea of the time domain that the data we have is interested in.
     if showprint or actuallyReturn:
         minT = df.time.min()
         maxT = df.time.max()
@@ -50,10 +50,11 @@ def timeStats(df, showprint=True, actuallyReturn=True):
         print maxT,   "Last Recording in this set"
         print rangeT, "Range covered"
     if actuallyReturn:
-        return (minT,maxT,rangeT)
+        return (minT, maxT, rangeT)
 
-if False:#SHOW_TESTS:
-    timeStats(bigdf,showprint=False, actuallyReturn=False)
+
+if False:  # SHOW_TESTS:
+    timeStats(bigdf, showprint=False, actuallyReturn=False)
     print
     timeStats(bigdf, actuallyReturn=False)
     print timeStats(bigdf, showprint=False), "\n"
@@ -68,28 +69,29 @@ def timeCropDf(df, startTime, windowLength):
     return df[(df.time > startTime) &
               (df.time < endTime)]
 
+
 if SHOW_TESTS:
     print "no tests written yet"
 
 
 def makeGif(fromPath, fileName, stepSize="", forReal=True, chatty=True):
-    #send a system command to make this into a gif. See https://community.linuxmint.com/tutorial/view/1118
-    command="convert -delay 20 -loop 0 {} {}{}.gif".format(fromPath, fileName, stepSize)
+    # send a system command to make this into a gif. See https://community.linuxmint.com/tutorial/view/1118
+    command = "convert -delay 20 -loop 0 {} {}{}.gif".format(fromPath, fileName, stepSize)
     if chatty:
         print command
-        print "made",fileName
+        print "made", fileName
     if forReal:
         return os.system(command)
+
 
 if SHOW_TESTS:
     print "no tests written yet"
 
 
-# In[9]:
-
 def clearFolder(path):
     mydir = os.getcwd() + path
-    map( os.unlink, [os.path.join( mydir,f) for f in os.listdir(mydir)] )
+    map(os.unlink, [os.path.join(mydir, f) for f in os.listdir(mydir)])
+
 
 if SHOW_TESTS:
     print "no tests written yet"
@@ -98,52 +100,60 @@ if SHOW_TESTS:
 # In[10]:
 
 def sydTimeToUTC(naiveTime):
-    ausSyd = pytz.timezone ("Australia/Sydney")
+    ausSyd = pytz.timezone("Australia/Sydney")
     local_dt = ausSyd.localize(naiveTime)
     utc_dt = local_dt.astimezone(pytz.utc)
     return utc_dt
 
+
 if SHOW_TESTS:
     for z in pytz.country_timezones['au']:
         print z
-    print              datetime.datetime(2016,10,17,11,21,32) , "Australia/Sydney"
-    print sydTimeToUTC(datetime.datetime(2016,10,17,11,21,32)), "UTC"
+    print              datetime.datetime(2016, 10, 17, 11, 21, 32), "Australia/Sydney"
+    print sydTimeToUTC(datetime.datetime(2016, 10, 17, 11, 21, 32)), "UTC"
 
 
 def getSampleWalkData(url):
-    firstRefTry = getDFfromCSVURL(url, columnNames=["stickerID","xstickerID","detectionType","xID","timeStamp"])
-    return firstRefTry.drop(["xstickerID","detectionType","xID"], 1)
+    firstRefTry = getDFfromCSVURL(url, columnNames=["stickerID", "xstickerID", "detectionType", "xID", "timeStamp"])
+    return firstRefTry.drop(["xstickerID", "detectionType", "xID"], 1)
+
 
 def getTrainingWalkData(trainingWalkURLs, stickerLocations):
     trainingWalks = []
     for i, twURL in enumerate(trainingWalkURLs):
         thisData = getSampleWalkData(twURL["url"])
-        richStickerData = thisData.merge(stickerLocations,on="stickerID")
-        richStickerData['time'] = richStickerData.apply(lambda x: sydTimeToUTC(dateutil.parser.parse(x.timeStamp, dayfirst=True)) , axis=1)
+        richStickerData = thisData.merge(stickerLocations, on="stickerID")
+        richStickerData['time'] = richStickerData.apply(lambda x: sydTimeToUTC(dateutil.parser.parse(x.timeStamp, dayfirst=True)), axis=1)
     #     richStickerData['sydtime'] = richStickerData.apply(lambda x: dateutil.parser.parse(x.timeStamp, dayfirst=True) , axis=1)
         richStickerData = richStickerData.drop(["timeStamp"], 1)
         richStickerData = richStickerData.sort_values("time", ascending=1)
         richStickerData["minor"] = twURL["minor"]
 
-        print "{}: {} TO {} ({}) {} rows".format(i, richStickerData.time.min(),
-                                                    richStickerData.time.max(),
-                                                    richStickerData.time.max() - richStickerData.time.min(),
-                                                    richStickerData.shape[0])
+        print "{}: {} TO {} ({}) {} rows".format(i,
+                                                 richStickerData.time.min(),
+                                                 richStickerData.time.max(),
+                                                 richStickerData.time.max() - richStickerData.time.min(),
+                                                 richStickerData.shape[0])
 
         trainingWalks.append(richStickerData)
     return trainingWalks
+
 
 class timeout:
     def __init__(self, seconds=1, error_message='Timeout'):
         self.seconds = seconds
         self.error_message = error_message
+
     def handle_timeout(self, signum, frame):
         raise TimeoutError(self.error_message)
+
     def __enter__(self):
         signal.signal(signal.SIGALRM, self.handle_timeout)
         signal.alarm(self.seconds)
+
     def __exit__(self, type, value, traceback):
         signal.alarm(0)
+
 
 def robustRequest(request, headers, tries=10, timeoutSeconds=20):
         response = False
@@ -158,6 +168,7 @@ def robustRequest(request, headers, tries=10, timeoutSeconds=20):
                             print "{}\nfailed {} times".format(request, i+1), sys.exc_info()[1]
                             return False
 
+
 def nSecondWindows(stickerRecordingsRow, baseStationData, chunkLength=5.0, chatty=True):
     try:
         startTime = datetime.datetime.now()
@@ -168,7 +179,7 @@ def nSecondWindows(stickerRecordingsRow, baseStationData, chunkLength=5.0, chatt
         we = row.time + halfWindow
         personID = row.minor
         url = "http://ec2-52-65-111-92.ap-southeast-2.compute.amazonaws.com:3000"
-        headers = {'authorization': "Basic ",# + key,
+        headers = {'authorization': "Basic ",  # + key,
                    'content-type':  "application/json",
                    'cache-control': "no-cache"}
 
@@ -179,29 +190,29 @@ def nSecondWindows(stickerRecordingsRow, baseStationData, chunkLength=5.0, chatt
 
         if windowDetectionsResponse:
             responseJSON = json.loads(windowDetectionsResponse.text)[u'results'][0]
-            if len(responseJSON)>0:
+            if len(responseJSON) > 0:
                 responses = pd.DataFrame.from_dict(responseJSON)
-                responses['temptime'] = responses.apply(lambda x: dateutil.parser.parse(x.time, dayfirst=True) , axis=1)
+                responses['temptime'] = responses.apply(lambda x: dateutil.parser.parse(x.time, dayfirst=True), axis=1)
                 responses = responses.drop(['time'], 1)
-                responses = responses.rename(columns = {'temptime':'time'})
+                responses = responses.rename(columns={'temptime': 'time'})
                 responses = responses.sort_values("time", ascending=1)
 
-                responses = pd.merge(responses,baseStationData,on=["agentId"])
+                responses = pd.merge(responses, baseStationData, on=["agentId"])
                 responses = responses.drop(["agentId"], 1)
                 # rescale rssi between 0 and max in positive numbers. Bigger number means more powerful signal
-                minPower = -100 #min(responses.rssi)
+                minPower = -100  # min(responses.rssi)
                 responses['rssiAdj'] = responses.rssi - minPower
 
                 if chatty:
                     print "index: {}\n{}\nstart: {}\ndetection: {}\nend: {}\ntook: {}\nresult rows: {}\n".format(
-                        row.name,row, ws, row.time, we, datetime.datetime.now() - startTime, responses.shape[0])
+                        row.name, row, ws, row.time, we, datetime.datetime.now() - startTime, responses.shape[0])
 
                 return responses
             else:
                 return "no detections found, deal with this later"
 
         else:
-            #the request returned false, so so will we
+            # the request returned false, so so will we
             return "detection request failed, deal with this later"
     except:
         print "OH FUCK"
@@ -220,44 +231,44 @@ def timeSliced(fullRow, savePlace, contextDF, baseStationData, stickerLocations)
     title = "Detections for person {} around {}".format(personID, fullRow.time.strftime(timeFormatString))
     plt.title(title)
 
-    #--- general vvv
+    # --- general vvv
 
-    #put the image on the graph as an underlay
+    # put the image on the graph as an underlay
     img = imread("map.png")
     ax.imshow(img, zorder=0, extent=[-1300, 72000, -24000, 41000])
     #                                [left,  right, bottom, top  ]
 
-    #put the baseStations on the map
+    # put the baseStations on the map
     for index, row in baseStationData.iterrows():
         ax.text(row.x, row.y, row.agentName, fontsize=8)
-    ax.scatter(baseStationData.x,baseStationData.y, marker='+')
+    ax.scatter(baseStationData.x, baseStationData.y, marker='+')
 
-    #Hide the axis numbers because they don't tell us much
+    # Hide the axis numbers because they don't tell us much
     ax.xaxis.set_major_formatter(plt.NullFormatter())
     ax.yaxis.set_major_formatter(plt.NullFormatter())
 
-    #--- specific to this animation vvv
+    # --- specific to this animation vvv
 
-    #the sticker labels
+    # the sticker labels
     for index, row in contextDF.iterrows():
         ax.text(row.x, row.y, "{0:03d}".format(row.stickerID), fontsize=8)
 
-    #sticker dots
+    # sticker dots
     ax.scatter(contextDF.x, contextDF.y, zorder=20)
-    ax.scatter(stickerLocations.x,stickerLocations.y,s=1,c="y")
+    ax.scatter(stickerLocations.x, stickerLocations.y, s=1, c="y")
 
-    #the path
+    # the path
     ax.plot(contextDF.x, contextDF.y, alpha=0.7, linewidth=1, c="g", solid_capstyle='round')
 
-    #--- specific to this frame vvv
+    # --- specific to this frame vvv
 
     # Plot all the detections onto the map as pale circles
     ax.scatter(df.x, df.y, zorder=1, s=abs(df.rssiAdj)*100, alpha=0.2)
 
-    #plot the current window's dot bigger
+    # plot the current window's dot bigger
     ax.scatter(fullRow.x, fullRow.y, zorder=30, s=150, c="r")
 
-    #--- clean up vvv
+    # --- clean up vvv
     print "saving to:", savePlace+title+".png"
     fig.savefig(savePlace+title+".png", bbox_inches='tight')
     fig.clf()
