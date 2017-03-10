36dd6db05dbe1b579ac9ce712942afeeeb07ed0d
Ben Doherty <ben@notionparallax.co.uk>
2017-03-10 17:03:41 +1100

    make a test setup

--- indoorSoftware/Python/test.py
+++ indoorSoftware/Python/test.py
@@ -1,21 +1,24 @@
 """Send lots of commands to the S^3."""
-import send
 import receive
-import time
+import send
 import sys
+
 
 def two_hex(intVal):
     """Convert int to simple hex."""
     return str(hex(intVal)[2:])
 
+
 def sixChar(r, g, b):
-    "Take 3 ints and return a value like BADA55."
+    """Take 3 ints and return a value like BADA55."""
     r = two_hex(r).rjust(2, "0")
     g = two_hex(g).rjust(2, "0")
     b = two_hex(b).rjust(2, "0")
     return "{}{}{}".format(r, g, b)
 
+
 def get_temp():
+    """Ask for a value from the thermistor."""
     send.send("t")
     send.send("t")
     receive.receive()
