de62eae02573f1f416e223fd6bb55cc947f53751
Ben Doherty <ben@notionparallax.co.uk>
2017-03-10 15:54:33 +1100

    modify to use local usb py

--- indoorSoftware/Python/read.py
+++ indoorSoftware/Python/read.py
@@ -1,5 +1,4 @@
 #/usr/bin/python
-
 """
 Written for PyUSB 1.0 (w/libusb 1.0.3)
 
@@ -13,17 +12,17 @@ Assumes 'UsbStreamDemo1.pde' is loaded on Arduino and
 LEDs are present on pins 11, 12 and 13.
 """
 
-import usb  # 1.0 not 0.4
+# import usb  # 1.0 not 0.4
 import sys
-from arduino.usbdevice import ArduinoUsbDevice
 import time
-
-sys.path.append("..")
+from usbdevice import atmelUsbDevice
 
 
 def read():
+    print "reading"
     try:
-        theDevice = ArduinoUsbDevice(idVendor=0x16c0, idProduct=0x05df)
+        theDevice = atmelUsbDevice(idVendor=0x16c0,
+                                   idProduct=0x05df)
 
         print "Found: 0x%04x 0x%04x %s %s" % (theDevice.idVendor,
                                               theDevice.idProduct,
@@ -34,7 +33,8 @@ def read():
 
     while True:
         try:
-            theDevice = ArduinoUsbDevice(idVendor=0x16c0, idProduct=0x05df)
+            theDevice = atmelUsbDevice(idVendor=0x16c0,
+                                       idProduct=0x05df)
             try:
                 sys.stdout.write(chr(theDevice.read()))
                 sys.stdout.flush()
--- indoorSoftware/Python/send.py
+++ indoorSoftware/Python/send.py
@@ -1,31 +1,25 @@
 #/usr/bin/python
+""" Written for PyUSB 1.0 (w/libusb 1.0.3)"""
 
-#
-# Written for PyUSB 1.0 (w/libusb 1.0.3)
-#
-
-
-import usb, sys # 1.0 not 0.4
-sys.path.append("..")
-
-from arduino.usbdevice import ArduinoUsbDevice
+import sys
+from usbdevice import atmelUsbDevice
 
 
 def send():
-   try:
-      theDevice = ArduinoUsbDevice(idVendor=0x16c0, idProduct=0x05df)
-   except:
-      sys.exit("No DigiUSB Device Found")
+    """Send a command to the sensicorn stampeed stick."""
+    try:
+        theDevice = atmelUsbDevice(idVendor=0x16c0, idProduct=0x05df)
+    except:
+        sys.exit("No DigiUSB Device Found")
 
+    try:
+        user_input = sys.argv[1]+"\n"
+    except:
+        exit("No data to send")
 
+    for c in user_input:
+        theDevice.write(ord(c))
 
-   try:
-     user_input = sys.argv[1]+"\n"
-   except:
-     exit("No data to send")
-        
-   for c in user_input:
-     theDevice.write(ord(c))
 
 if __name__ == "__main__":
-  send()
+    send()
