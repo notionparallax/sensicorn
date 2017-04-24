fb1b41e0464172e191dd2d03a3a73dfdf7de1acb
AidenRay <aidenunsw@outlook.com>
2017-04-25 00:51:52 +1000

    minor changes

--- indoorSoftware/Python/plotEnvelope.py
+++ indoorSoftware/Python/plotEnvelope.py
@@ -21,10 +21,10 @@ plt.plot(t,sound)
 '''
 Downsample signal by a factor of 30, i.e. take every 30th sample
 '''
-downsample = 5
-sound2 = signal.decimate(sound,downsample)
-t = np.linspace(0,120,len(sound2))
-plt.plot(t,sound2)
+# downsample = 5
+# sound2 = signal.decimate(sound,downsample)
+# t = np.linspace(0,120,len(sound2))
+# plt.plot(t,sound2)
 
 downsample = 30
 sound3 = signal.decimate(sound,downsample)
